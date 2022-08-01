7d4e9c7c3fac5f235da6beb69177734d02f21150
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2022-08-01 15:14:45 +0800

    Change main website images to absolute path

--- _nuxt/default-169f2dbe.mjs
+++
@@ -1,1 +0,0 @@
-import{S as r,o as a,a as t,l as o}from"./entry-2229627f.mjs";const l={};function n(e,s){return a(),t("div",null,[o(e.$slots,"default")])}var f=r(l,[["render",n]]);export{f as default};
---
+++ _nuxt/padded-e3770146.mjs
@@ -0,0 +1,1 @@
+import{S as s,o as t,a,l as o}from"./entry-8dced93c.mjs";const r={},c={class:"mt-24"};function n(e,d){return t(),a("div",c,[o(e.$slots,"default")])}var _=s(r,[["render",n]]);export{_ as default};
