a54dff6b974416a0fc0200c978faeb2e5e2db6f8
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2023-01-22 13:17:38 +0800

    Prefetch links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://jaezmien.github.io',
-	integrations: [sitemap(), tailwind()],
+  site: 'https://jaezmien.github.io',
+  integrations: [sitemap(), tailwind(), prefetch()]
 });
