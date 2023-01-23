aef5fd090d119aac75dc13a62f00f8df67393b0f
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2023-01-23 16:48:29 +0800

    Convert KoFi script into a component

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import prefetch from "@astrojs/prefetch";
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jaezmien.github.io',
-  integrations: [sitemap(), tailwind(), prefetch()]
+	site: 'https://jaezmien.github.io',
+	integrations: [sitemap(), tailwind(), prefetch()],
 });
